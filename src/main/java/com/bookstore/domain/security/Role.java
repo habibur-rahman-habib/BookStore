03ba98f7aa3f3eff0260100b15efc0af7e455760
habibur-rahman-habib <habibur0199mhr@gmail.com>
2023-05-28 00:49:42 +0600

    working on security or domain

---
+++ src/main/java/com/bookstore/domain/security/Role.java
@@ -0,0 +1,22 @@
+package com.bookstore.domain.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Role {
+
+	@Id
+	private int roleId;
+	private String name;
+	
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+}
